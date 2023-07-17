3bc4c8e43fd1f094afe17372f042a82a3971e258
stell <stell.ok@outlook.com>
2023-07-17 14:27:12 +0800

    Change param

--- migrations/1_bidding_deploy_contracts.js
+++ migrations/1_bidding_deploy_contracts.js
@@ -2,26 +2,31 @@ const Bidding = artifacts.require("Bidding");
 const USDT = artifacts.require("Usdt");
 const Financing = artifacts.require("Financing");
 
-module.exports = async function(deployer,network,accounts) {
+const Web3 = require('web3');
+
+module.exports = async function (deployer, network, accounts) {
 
     //deployment usdt
-    await deployer.deploy(USDT, 100000000000);
+    await deployer.deploy(USDT, web3.utils.toWei('10000000000', 'ether'));
     //access information about your deployed contract instance
     const usdtContract = await USDT.deployed();
     console.log(`USDT contract ${usdtContract.address}`)
     const usdtBalance = await usdtContract.balanceOf(accounts[0]);
     console.log(`owner ${accounts[0]}`)
-    console.log(`Owner USDT Balance: ${usdtBalance}`)
+    console.log(`Owner USDT Balance: ${web3.utils.fromWei(usdtBalance, 'ether')}`)
 
     //deploy bidding
     await deployer.deploy(Bidding,
-        usdtContract.address, // IERC20 usdtAddr_
-        accounts[0],          // address owner_,
-        accounts[0],          // address founderAddr_
-        accounts[0],          // address adminAddr_
-        1,                    // service fee
-        7,                    // dd fee
-        accounts[0])          // address ddAddr_
+        usdtContract.address,                     // IERC20 usdtAddr_
+        accounts[0],                              // address owner_,
+        accounts[0],                              // address founderAddr_
+        accounts[0],                              // address adminAddr_
+        web3.utils.toWei('10000', 'ether'),       // service fee
+        web3.utils.toWei('90000', 'ether'),       // dd fee
+        accounts[0],                              // address ddAddr_
+        accounts[1]                               // platformFeeAddr_
+    )
+
     const bidContract = await Bidding.deployed()
     console.log(`bidding contract ${bidContract.address}`)
 
