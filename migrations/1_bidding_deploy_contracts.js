8ef28fa8c859c201955b3b2f1e84c45da4f03b51
stell <stell.ok@outlook.com>
2023-07-19 17:37:28 +0800

    Add network local

--- migrations/1_bidding_deploy_contracts.js
+++ migrations/1_bidding_deploy_contracts.js
@@ -6,18 +6,27 @@ const Web3 = require('web3');
 
 module.exports = async function (deployer, network, accounts) {
 
-    //deployment usdt
-    await deployer.deploy(USDT, web3.utils.toWei('10000000000', 'ether'));
-    //access information about your deployed contract instance
-    const usdtContract = await USDT.deployed();
-    console.log(`USDT contract : ${usdtContract.address}`)
-    const usdtBalance = await usdtContract.balanceOf(accounts[0]);
-    console.log(`owner : ${accounts[0]}`)
-    console.log(`Owner USDT Balance: ${web3.utils.fromWei(usdtBalance, 'ether')}`)
+    let usdt = '0xed269cACd679309FAC6132F2A773B3d49535Dc87'
+    if (network === 'development') {
+        //deployment usdt
+        await deployer.deploy(USDT, web3.utils.toWei('10000000000', 'ether'));
+        //access information about your deployed contract instance
+        const usdtContract = await USDT.deployed();
+        console.log(`USDT contract : ${usdtContract.address}`)
+        const usdtBalance = await usdtContract.balanceOf(accounts[0]);
+        console.log(`owner : ${accounts[0]}`)
+        console.log(`Owner USDT Balance: ${web3.utils.fromWei(usdtBalance, 'ether')}`)
+        usdt = usdtContract.address
+    }
+
+    if (network === 'local'){
+        usdt = '0xed269cACd679309FAC6132F2A773B3d49535Dc87'
+    }
+
 
     //deploy bidding
     await deployer.deploy(Bidding,
-        usdtContract.address,                     // IERC20 usdtAddr_
+        usdt,                                     // IERC20 usdtAddr_
         accounts[0],                              // address founderAddr_
         accounts[0],                              // address adminAddr_
         web3.utils.toWei('10000', 'ether'),       // service fee
--- truffle-config.js
+++ truffle-config.js
@@ -44,7 +44,7 @@
 // require('dotenv').config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
     /**
@@ -56,6 +56,9 @@ module.exports = {
      *
      * $ truffle test --network <network-name>
      */
+    plugins: [
+        "@chainsafe/truffle-plugin-abigen"
+    ],
 
     networks: {
         // Useful for testing. The `development` name is special - truffle uses it by default
@@ -68,6 +71,21 @@ module.exports = {
             host: "127.0.0.1",     // Localhost (default: none)
             port: 8545,            // Standard Ethereum port (default: none)
             network_id: "*",       // Any network (default: none)
+        },
+
+        my: {
+            host: "192.168.1.115",     // Localhost (default: none)
+            port: 8545,            // Standard Ethereum port (default: none)
+            network_id: "*",       // Any network (default: none)
+        },
+
+        //http://192.168.1.251:8549
+        local: {
+          provider: () => new HDWalletProvider('during retreat copy liberty gauge acquire twenty nice today dry leader speed', `http://192.168.1.251:8549/`),
+          network_id: '*',       // Goerli's id
+          confirmations: 1,    // # of confirmations to wait between deployments. (default: 0)
+          timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+          skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
         },
         //
         // An additional network, but with some advanced options…
