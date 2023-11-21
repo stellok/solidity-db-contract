d565e19e29bfe91af4a54fca51da14e4bf43cf99
safe <safe.ok@outlook.com>
2023-11-21 14:46:45 +0800

    Remove unused import

--- contracts/Usdt.sol
+++ contracts/Usdt.sol
@@ -2,13 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Usdt is ERC20 {
+contract Usdt is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("USDTF", "USDTF") {
         _mint(msg.sender, initialSupply);
     }
 
     function decimals() public pure override returns (uint8) {
         return 6;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
--- contracts/referral/UserNft.sol
+++ contracts/referral/UserNft.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
